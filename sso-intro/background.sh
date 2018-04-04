5046fd35f709947f6575fdbdf19d98bd29e8f9cc
Joel Speed <Joel.speed@hotmail.co.uk>
2018-04-04 11:36:57 +0100

    Substitute client side?

--- sso-intro/background.sh
+++ sso-intro/background.sh
@@ -1,1 +1,7 @@
 ssh root@host01 "sed -i \"s/\[\[KATACODA_HOST\]\]/\$KATACODA_HOST/g\" dex.yaml && sed -i \"s/\[\[HOST_SUBDOMAIN\]\]/\$HOST_SUBDOMAIN/g\" dex.yaml"
+sed -i "s/\[\[KATACODA_HOST\]\]/$KATACODA_HOST/g" dex.yaml && sed -i "s/\[\[HOST_SUBDOMAIN\]\]/$HOST_SUBDOMAIN/g" dex.yaml
+scp dex.yaml root@host01:/root/dex2.yaml
+
+# Debugging
+env > env.conf
+scp env.conf root@host01:/root/env.conf
